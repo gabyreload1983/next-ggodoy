48f3b75274036c296ef32e70327d46e5c7b1ee64
Gabriel Godoy <gabyreload@gmail.com>
2024-03-09 12:06:15 -0300

    styling grid projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,11 +5,11 @@ export default async function Projects() {
   const projects = await getProjects();
 
   return (
-    <main className="p-5 md:w-5/6 md:mx-auto">
-      <div className="border-b-2 border-primary p-5 max-w-6xl mx-auto mb-10">
+    <main className="p-10 md:max-w-screen-2xl md:mx-auto">
+      <div className="border-b-2 border-primary pb-5 max-w-6xl mx-auto mb-10">
         <p className="text-2xl">This is what I do... and love</p>
       </div>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
         {projects.map((p) => (
           <Card key={p.id} project={p} />
         ))}
